cc5a8c2a3c2daf3d98b65640fab239e24af0b975
Bob Davies <bobdavies2000@gmail.com>
2021-02-28 23:08:20 -0800

    Uncommented the code to show the extrinsics.  Now that the Python cameras are disabled, there is no problem.

--- VB_Classes/extrinsics.vb
+++ VB_Classes/extrinsics.vb
@@ -8,15 +8,15 @@ Public Class Extrinsics_Basics
     Public Sub Run()
 		If task.intermediateReview = caller Then ocvb.intermediateObject = Me
         Dim nextline = "Rotation MatrixTranslation" + vbCrLf
-        'Dim fmt = "#0.0000"
-        'nextline += Format(ocvb.parms.extrinsics.rotation(0), fmt) + vbTab + Format(ocvb.parms.extrinsics.rotation(1), fmt) + vbTab +
-        '               Format(ocvb.parms.extrinsics.rotation(2), fmt) + vbTab + vbTab + vbTab + Format(ocvb.parms.extrinsics.translation(0), fmt) + vbCrLf
+        Dim fmt = "#0.0000"
+        nextline += Format(ocvb.parms.extrinsics.rotation(0), fmt) + vbTab + Format(ocvb.parms.extrinsics.rotation(1), fmt) + vbTab +
+                       Format(ocvb.parms.extrinsics.rotation(2), fmt) + vbTab + vbTab + vbTab + Format(ocvb.parms.extrinsics.translation(0), fmt) + vbCrLf
 
-        'nextline += Format(ocvb.parms.extrinsics.rotation(3), fmt) + vbTab + Format(ocvb.parms.extrinsics.rotation(4), fmt) + vbTab +
-        '               Format(ocvb.parms.extrinsics.rotation(5), fmt) + vbTab + vbTab + vbTab + Format(ocvb.parms.extrinsics.translation(1), fmt) + vbCrLf
+        nextline += Format(ocvb.parms.extrinsics.rotation(3), fmt) + vbTab + Format(ocvb.parms.extrinsics.rotation(4), fmt) + vbTab +
+                       Format(ocvb.parms.extrinsics.rotation(5), fmt) + vbTab + vbTab + vbTab + Format(ocvb.parms.extrinsics.translation(1), fmt) + vbCrLf
 
-        'nextline += Format(ocvb.parms.extrinsics.rotation(6), fmt) + vbTab + Format(ocvb.parms.extrinsics.rotation(7), fmt) + vbTab +
-        '               Format(ocvb.parms.extrinsics.rotation(8), fmt) + vbTab + vbTab + vbTab + Format(ocvb.parms.extrinsics.translation(2), fmt) + vbCrLf
+        nextline += Format(ocvb.parms.extrinsics.rotation(6), fmt) + vbTab + Format(ocvb.parms.extrinsics.rotation(7), fmt) + vbTab +
+                       Format(ocvb.parms.extrinsics.rotation(8), fmt) + vbTab + vbTab + vbTab + Format(ocvb.parms.extrinsics.translation(2), fmt) + vbCrLf
         ocvb.trueText(nextline)
     End Sub
 End Class
