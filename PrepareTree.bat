0cea8fddf71dfe09503ebab68b996c605d9cd554
Bob Davies <bobdavies2000@gmail.com>
2021-03-06 15:51:44 -0800

    Skip trying to open the OpenCVB.sln as part of the install.  Not necessary.

--- PrepareTree.bat
+++ PrepareTree.bat
@@ -22,4 +22,3 @@ msbuild.exe librealsense/Build/librealsense2.sln /p:Configuration=Release
 
 msbuild.exe Azure-Kinect-Sensor-SDK/Build/k4a.sln /p:Configuration=Debug
 msbuild.exe Azure-Kinect-Sensor-SDK/Build/k4a.sln /p:Configuration=Release
-start OpenCV.sln
