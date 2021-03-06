052a2957fd02c48cf44dc0713a66e40149415163
Bob Davies <bobdavies2000@gmail.com>
2021-03-06 08:44:56 -0800

    Moving things around in the PrepareTree.bat file - no real change, just put all the compiles at the end.

--- PrepareTree.bat
+++ PrepareTree.bat
@@ -5,15 +5,16 @@ cd OpenCV
 
 cd ..\		
 "C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DWITH_OPENGL=ON -DBUILD_EXAMPLES=OFF -DCPU_DISPATCH=SSE4_1;SSE4_2;AVX;FP16 -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_opencv_python_tests=OFF -DOPENCV_EXTRA_MODULES_PATH=OpenCV/OpenCV_Contrib/Modules -S OpenCV -B OpenCV/Build
-start OpenCV/Build/OpenCV.sln
 
 if exist librealsense (rmdir librealsense /s)
 "c:\Program Files\Git\bin\git.exe" clone "https://github.com/IntelRealSense/librealsense"
 "C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DBUILD_CSHARP_BINDINGS=1 -DBUILD_CV_EXAMPLES=0 -S librealsense -B librealsense/Build
-start librealsense/Build/librealsense2.sln
 
 if exist Azure-Kinect-Sensor-SDK (rmdir Azure-Kinect-Sensor-SDK /s)
 "c:\Program Files\Git\bin\git.exe" clone "https://github.com/microsoft/Azure-Kinect-Sensor-SDK"
 "C:\Program Files\CMake\bin\Cmake.exe" -DCMAKE_CONFIGURATION_TYPES=Debug;Release -DOpenCV_DIR=OpenCV/Build -DCMAKE_BUILD_TYPE=Debug -S Azure-Kinect-Sensor-SDK -B Azure-Kinect-Sensor-SDK/Build
+
+start OpenCV/Build/OpenCV.sln
+start librealsense/Build/librealsense2.sln
 start Azure-Kinect-Sensor-SDK/Build/k4a.sln
 
