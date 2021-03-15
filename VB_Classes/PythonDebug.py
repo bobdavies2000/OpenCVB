d754344c62ef97cd7db3eb25b76017b590d49d7a
Bob Davies <bobdavies2000@gmail.com>
2021-03-14 20:18:06 -0700

    Improved the MPL_PlotFunction_PS.py algorithm to show more data per iteration.

--- VB_Classes/PythonDebug.py
+++ VB_Classes/PythonDebug.py
@@ -5,30 +5,31 @@ import matplotlib.animation as animation
 from PyStream import PyStreamRun
 import cv2 as cv
 import io
-titleWindow = 'MPL_Animate_PS.py'
+titleWindow = 'MPL_PlotFunction_PS.py'
 
 def OpenCVCode(imgRGB, depth32f, frameCount):
-    global xdata, ydata, ax, line
+    global xdata, ydata, line, maxX
     fig, ax = plt.subplots()
     if frameCount == 0: 
-        xdata, ydata = [], []
         line, = ax.plot([], [], lw=2)    # update the data
         ax.set_ylim(-1.1, 1.1)
-        ax.set_xlim(0, 10)
-
-    ax.grid()
-    t = frameCount / 10
-
-    xdata.append(t)
-    ydata.append(np.sin(2*np.pi*t) * np.exp(-t/10.))
+        maxX = 10
+    
+    for i in range(0, 9):
+        t = frameCount + i / 10
+        xdata.append(t)
+        ydata.append(np.sin(2*np.pi*t) * np.exp(-t/10.))
     line.set_data(xdata, ydata)
-    xmin, xmax = ax.get_xlim()
 
-    if t >= xmax:
-        ax.set_xlim(0, 2*t)
+    if frameCount >= maxX:
+        maxX *= 2
+        ax.set_xlim(0, maxX)
         ax.figure.canvas.draw()
 
+    ax.set_xlim(0, maxX)
+    ax.grid()
     plt.plot(xdata, ydata)
+
     buf = io.BytesIO()
     plt.savefig(buf, format='rgba', dpi=100)
 
@@ -39,4 +40,5 @@ def OpenCVCode(imgRGB, depth32f, frameCount):
     plt.close()
     return imgRGB, tmp
 
+xdata, ydata = [], []
 PyStreamRun(OpenCVCode, titleWindow)
