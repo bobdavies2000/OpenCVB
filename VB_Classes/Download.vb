4d43a11cd52e4791595668fe49a074c8610400aa
Bob Davies <bobdavies2000@gmail.com>
2021-05-15 08:38:33 -0700

    Added a download all feature to the download algorithm.

--- VB_Classes/Download.vb
+++ VB_Classes/Download.vb
@@ -8,6 +8,10 @@ Public Class Download_Databases : Inherits VBparent
     Dim linkAddress As String = ""
     Dim zippedBuffer As New MemoryStream
     Public Sub New()
+        If findfrm(caller + " CheckBox Options") Is Nothing Then
+            check.Setup(caller, 1)
+            check.Box(0).Text = "Download All"
+        End If
         If radio.Setup(caller, 9) Then
             radio.check(0).Text = "Download the 1.7 Gb 300 Faces In-The-Wild database"
             radio.check(1).Text = "Download TensorFlow MobileNet-SSD v1"
@@ -20,39 +24,41 @@ Public Class Download_Databases : Inherits VBparent
             radio.check(8).Text = "Download TensorFlow Mask-RCNN Inception v2"
             radio.check(6).Checked = True
         End If
-
         task.desc = "Multi-threaded (responsive) download of the iBug 300W face database.  Not using iBug yet but planning to..."
     End Sub
     Public Sub Run(src As cv.Mat) ' Rank = 1
-        Static checkiBug = findRadio("Download the 1.7 Gb 300 Faces In-The-Wild database")
-        Static checkTensor1 = findRadio("Download TensorFlow MobileNet-SSD v1")
-        Static checkTensor2 = findRadio("Download TensorFlow MobileNet-SSD v1 PPN")
-        Static checkTensor3 = findRadio("Download TensorFlow MobileNet-SSD v2")
-        Static checkTensor4 = findRadio("Download TensorFlow Inception-SSD v2")
-        Static checkTensor5 = findRadio("Download TensorFlow MobileNet-SSD v3")
-        Static checkTensor6 = findRadio("Download TensorFlow Faster-RCNN Inception v2")
-        Static checkTensor7 = findRadio("Download TensorFlow Faster-RCNN ResNet-50")
-        Static checkTensor8 = findRadio("Download TensorFlow Mask-RCNN Inception v2")
+        Static downloadAllCheck = findCheckBox("Download All")
+        If downloadAllCheck.checked Then
+            Static dIndex As Integer
+            If downloadActive = False Then
+                radio.check(dIndex).Checked = True
+                dIndex += 1
+                If dIndex >= radio.check.Count - 1 Then
+                    downloadAllCheck.checked = False
+                    dIndex = 0
+                End If
+            End If
+        End If
 
-        If checkiBug.checked Then linkAddress = "http://dlib.net/files/data/ibug_300W_large_face_landmark_dataset.tar.gz"
-        If checkTensor1.checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_2017_11_17.tar.gz"
-        If checkTensor2.checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_ppn_shared_box_predictor_300x300_coco14_sync_2018_07_03.tar.gz"
-        If checkTensor3.checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v2_coco_2018_03_29.tar.gz"
-        If checkTensor4.checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_2017_11_17.tar.gz"
-        If checkTensor5.checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v3_large_coco_2020_01_14.tar.gz"
-        If checkTensor6.checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/faster_rcnn_inception_v2_coco_2018_01_28.tar.gz"
-        If checkTensor7.checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet50_coco_2018_01_28.tar.gz"
-        If checkTensor8.checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/mask_rcnn_inception_v2_coco_2018_01_28.tar.gz"
+        If radio.check(0).Checked Then linkAddress = "http://dlib.net/files/data/ibug_300W_large_face_landmark_dataset.tar.gz"
+        If radio.check(1).Checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_2017_11_17.tar.gz"
+        If radio.check(2).Checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_ppn_shared_box_predictor_300x300_coco14_sync_2018_07_03.tar.gz"
+        If radio.check(3).Checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v2_coco_2018_03_29.tar.gz"
+        If radio.check(4).Checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_2017_11_17.tar.gz"
+        If radio.check(5).Checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v3_large_coco_2020_01_14.tar.gz"
+        If radio.check(6).Checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/faster_rcnn_inception_v2_coco_2018_01_28.tar.gz"
+        If radio.check(7).Checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet50_coco_2018_01_28.tar.gz"
+        If radio.check(8).Checked Then linkAddress = "http://download.tensorflow.org/models/object_detection/mask_rcnn_inception_v2_coco_2018_01_28.tar.gz"
 
         Dim filename = "ibug_300W_large_face_landmark_dataset.tar.gz"
-        If checkTensor1.checked Then filename = "ssd_mobilenet_v1_coco_2017_11_17.tar.gz"
-        If checkTensor2.checked Then filename = "ssd_mobilenet_v1_ppn_shared_box_predictor_300x300_coco14_sync_2018_07_03.tar.gz"
-        If checkTensor3.checked Then filename = "ssd_mobilenet_v2_coco_2018_03_29.tar.gz"
-        If checkTensor4.checked Then filename = "ssd_inception_v2_coco_2017_11_17.tar.gz"
-        If checkTensor5.checked Then filename = "ssd_mobilenet_v3_large_coco_2020_01_14.tar.gz"
-        If checkTensor6.checked Then filename = "faster_rcnn_inception_v2_coco_2018_01_28.tar.gz"
-        If checkTensor7.checked Then filename = "faster_rcnn_resnet50_coco_2018_01_28.tar.gz"
-        If checkTensor8.checked Then filename = "mask_rcnn_inception_v2_coco_2018_01_28.tar.gz"
+        If radio.check(1).Checked Then filename = "ssd_mobilenet_v1_coco_2017_11_17.tar.gz"
+        If radio.check(2).Checked Then filename = "ssd_mobilenet_v1_ppn_shared_box_predictor_300x300_coco14_sync_2018_07_03.tar.gz"
+        If radio.check(3).Checked Then filename = "ssd_mobilenet_v2_coco_2018_03_29.tar.gz"
+        If radio.check(4).Checked Then filename = "ssd_inception_v2_coco_2017_11_17.tar.gz"
+        If radio.check(5).Checked Then filename = "ssd_mobilenet_v3_large_coco_2020_01_14.tar.gz"
+        If radio.check(6).Checked Then filename = "faster_rcnn_inception_v2_coco_2018_01_28.tar.gz"
+        If radio.check(7).Checked Then filename = "faster_rcnn_resnet50_coco_2018_01_28.tar.gz"
+        If radio.check(8).Checked Then filename = "mask_rcnn_inception_v2_coco_2018_01_28.tar.gz"
 
         Dim fileToDecompress = New FileInfo(task.parms.homeDir + "Data/" + filename)
         Dim downloadDir = New DirectoryInfo(task.parms.homeDir + "Data/" + Mid(fileToDecompress.Name, 1, Len(fileToDecompress.Name) - Len(".tar.gz")))
