4f3cd756e8fc1377f350b990059368fc5e949f8f
Bob Davies <bobdavies2000@gmail.com>
2021-07-06 20:25:17 -0700

    Consolidate all the rmdir's into the start of the PrepareTree.bat.

--- PrepareTree.bat
+++ PrepareTree.bat
@@ -1,4 +1,9 @@
+if exist Azure-Kinect-Sensor-SDK (rmdir Azure-Kinect-Sensor-SDK /s)
+if exist opencv (rmdir Azure-Kinect-Sensor-SDK /s)
+if exist librealsense (rmdir Azure-Kinect-Sensor-SDK /s)
+
 cd support
+
 prepareOpenCV.bat
 PrepareLibrealsense.bat
 PrepareAzure4Kinect.bat
