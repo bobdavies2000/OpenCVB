837b096aedf29e83d22e063a194ca76b045cea54
Bob Davies <bobdavies2000@gmail.com>
2024-01-05 12:32:42 -0800

    VB_to_CPP translator start

--- VB_to_CPP/VB_to_CPP.Designer.vb
+++ VB_to_CPP/VB_to_CPP.Designer.vb
@@ -33,69 +33,74 @@ Partial Class VB_to_CPP
         'vbList
         '
         Me.vbList.FormattingEnabled = True
-        Me.vbList.Location = New System.Drawing.Point(12, 56)
+        Me.vbList.Location = New System.Drawing.Point(18, 86)
+        Me.vbList.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.vbList.Name = "vbList"
-        Me.vbList.Size = New System.Drawing.Size(322, 21)
+        Me.vbList.Size = New System.Drawing.Size(481, 28)
         Me.vbList.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 30)
+        Me.Label1.Location = New System.Drawing.Point(18, 46)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(1268, 23)
+        Me.Label1.Size = New System.Drawing.Size(624, 35)
         Me.Label1.TabIndex = 1
-        Me.Label1.Text = "VB.Net Algorithms - select one to translate to C++      NOTE: this is not a gener" &
-    "al purpose translator.  It is specific to OpenCVB VB.Net algorithms and only app" &
-    "roximate."
+        Me.Label1.Text = "VB.Net Algorithms - select one to translate to C++"
         '
         'VBrtb
         '
         Me.VBrtb.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.VBrtb.Location = New System.Drawing.Point(12, 124)
+        Me.VBrtb.Location = New System.Drawing.Point(1, 191)
+        Me.VBrtb.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.VBrtb.Name = "VBrtb"
-        Me.VBrtb.Size = New System.Drawing.Size(816, 978)
+        Me.VBrtb.Size = New System.Drawing.Size(995, 1439)
         Me.VBrtb.TabIndex = 2
         Me.VBrtb.Text = ""
         '
         'CPPrtb
         '
         Me.CPPrtb.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CPPrtb.Location = New System.Drawing.Point(834, 124)
+        Me.CPPrtb.Location = New System.Drawing.Point(1006, 191)
+        Me.CPPrtb.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.CPPrtb.Name = "CPPrtb"
-        Me.CPPrtb.Size = New System.Drawing.Size(1114, 978)
+        Me.CPPrtb.Size = New System.Drawing.Size(968, 1439)
         Me.CPPrtb.TabIndex = 3
         Me.CPPrtb.Text = ""
         '
         'Label2
         '
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(12, 98)
+        Me.Label2.Location = New System.Drawing.Point(18, 151)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(416, 23)
+        Me.Label2.Size = New System.Drawing.Size(624, 35)
         Me.Label2.TabIndex = 4
-        Me.Label2.Text = "VB.Net Code"
+        Me.Label2.Text = "Copy And paste this code into Google's Bard"
         '
         'Label3
         '
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(827, 98)
+        Me.Label3.Location = New System.Drawing.Point(1016, 151)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(416, 23)
+        Me.Label3.Size = New System.Drawing.Size(624, 35)
         Me.Label3.TabIndex = 5
-        Me.Label3.Text = "C++ Code (approximate)"
+        Me.Label3.Text = "C++ Code - Paste output of translate from Bard here "
         '
         'VB_to_CPP
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1807, 1061)
+        Me.ClientSize = New System.Drawing.Size(1970, 1632)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.CPPrtb)
         Me.Controls.Add(Me.VBrtb)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.vbList)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "VB_to_CPP"
         Me.Text = "VB_to_CPP - translate OpenCVB algorithms in VB.Net to an 'Include Only' C++ algor" &
     "ithm"
