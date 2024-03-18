3c6ab3acad27a15e40d231cda516b546a9038470
Bob Davies <bobdavies2000@gmail.com>
2024-03-18 11:28:42 -0700

    Converting HeatMap_Top/Side to use HeatMap_ObjectList

--- VB_Classes/HeatMap.vb
+++ VB_Classes/HeatMap.vb
@@ -201,7 +201,7 @@ End Class
 
 
 
-Public Class HeatMap_Top : Inherits VB_Algorithm
+Public Class HeatMap_Top1 : Inherits VB_Algorithm
     Dim histTop As New Histogram2D_Top
     Dim redC As New RedCloud_BasicsMask
     Dim redCells As New List(Of rcData)
@@ -243,6 +243,7 @@ End Class
 
 
 
+
 Public Class HeatMap_Side : Inherits VB_Algorithm
     Dim histSide As New Histogram2D_Side
     Dim redC As New RedCloud_BasicsMask
@@ -253,29 +254,40 @@ Public Class HeatMap_Side : Inherits VB_Algorithm
     End Sub
     Public Sub RunVB(src As cv.Mat)
         If task.heartBeat Then
+            Dim depthCount = task.pcSplit(2).CountNonZero
             histSide.Run(src)
             dst3 = histSide.dst2
 
             redC.inputMask = Not histSide.dst3
             redC.Run(histSide.dst3)
-            Dim cells As New List(Of rcData)(redC.redCells)
-            For Each rc In redC.redCells
-                For i = 0 To cells.Count - 1
-                    Dim rcBig = cells(i)
+
+            For i = 0 To redC.redCells.Count - 1
+                Dim rc = redC.redCells(i)
+                Dim tmp = New cv.Mat(rc.rect.Size, cv.MatType.CV_32F, 0)
+                dst3(rc.rect).CopyTo(tmp, rc.mask)
+                redC.redCells(i).pixels = tmp.Sum()
+            Next
+
+            For i = 0 To redC.redCells.Count - 1
+                Dim rcBig = redC.redCells(i)
+                For j = i + 1 To redC.redCells.Count - 1
+                    Dim rc = redC.redCells(j)
+                    If rc.pixels = 0 Then Continue For
                     If rcBig.rect.Contains(rc.rect) Then
                         rcBig.rect = rcBig.rect.Union(rc.rect)
-                        Dim sum1 = dst3(rcBig.rect).Sum()
-                        Dim sum2 = dst3(rc.rect).Sum()
-                        rcBig.pixels = sum1(0) + sum2(0)
-                        cells(i) = rcBig
-                        Exit For
+                        redC.redCells(i).pixels += rc.pixels
+                        redC.redCells(j).pixels = 0
                     End If
                 Next
             Next
 
+            Dim check1 = dst3.Sum()(0)
+
             Dim sortedCells As New SortedList(Of Integer, rcData)(New compareAllowIdenticalIntegerInverted)
-            For Each rc In cells
+            Dim check2 As Integer
+            For Each rc In redC.redCells
                 sortedCells.Add(rc.pixels, rc)
+                check2 += rc.pixels
             Next
 
             redCells.Clear()
@@ -284,24 +296,218 @@ Public Class HeatMap_Side : Inherits VB_Algorithm
                 rc.index = redCells.Count
                 redCells.Add(rc)
             Next
+
+            Dim otherCount As Integer
+            strOut = ""
+            For Each rc In redCells
+                If rc.index = 0 Then Continue For
+                If rc.index <= redOptions.identifyCount Then
+                    dst2.Rectangle(rc.rect, task.highlightColor, task.lineWidth)
+                    Dim y1 = (task.rangesSide(0).End - task.rangesSide(0).Start) * rc.rect.Y / dst2.Height
+                    Dim y2 = (task.rangesSide(0).End - task.rangesSide(0).Start) * (rc.rect.Y + rc.rect.Height) / dst2.Height
+                    Dim z1 = (task.rangesSide(1).End - task.rangesSide(1).Start) * rc.rect.X / dst2.Width
+                    Dim z2 = (task.rangesSide(1).End - task.rangesSide(1).Start) * (rc.rect.X + rc.rect.Width) / dst2.Width
+                    strOut += "Object " + vbTab + CStr(rc.index) + vbTab + Format(y2 - y1, fmt3) + " m high " + vbTab +
+                               Format(z1, fmt1) + "m to " + Format(z2, fmt1) + "m from camera" + vbTab + CStr(rc.pixels) + " pixels" + vbCrLf
+                Else
+                    otherCount += rc.pixels
+                End If
+            Next
+
+            strOut += "Other  " + vbTab + CStr(otherCount) + " pixels" + vbCrLf
+            strOut += "Total  " + vbTab + CStr(depthCount) + " pixels" + vbCrLf
+            strOut += "Check1 " + vbTab + CStr(depthCount) + " pixels" + vbCrLf
+            strOut += "Check2 " + vbTab + CStr(depthCount) + " pixels" + vbCrLf
         End If
+        setTrueText(strOut, 3)
 
         dst2 = redC.dst2
-        Dim ttY As Integer = 10
-        For Each rc In redCells
-            If rc.index = 0 Then Continue For
-            dst2.Rectangle(rc.rect, task.highlightColor, task.lineWidth)
-            If rc.index < redOptions.identifyCount Then setTrueText(CStr(rc.index), New cv.Point(rc.rect.X - 10, rc.rect.Y))
-            Dim y1 = (task.rangesSide(0).End - task.rangesSide(0).Start) * rc.rect.Y / dst2.Height
-            Dim y2 = (task.rangesSide(0).End - task.rangesSide(0).Start) * (rc.rect.Y + rc.rect.Height) / dst2.Height
-            Dim z1 = (task.rangesSide(1).End - task.rangesSide(1).Start) * rc.rect.X / dst2.Width
-            Dim z2 = (task.rangesSide(1).End - task.rangesSide(1).Start) * (rc.rect.X + rc.rect.Width) / dst2.Width
-            setTrueText("Object " + vbTab + CStr(rc.index) + vbTab + Format(y2 - y1, fmt1) + "m wide " + vbTab +
-                        Format(z1, fmt1) + "m to " + Format(z2, fmt1) + "m from camera" + vbTab + CStr(rc.pixels) + " pixels",
-                        New cv.Point(10, ttY), 3)
-            ttY += 10
-            If rc.index >= redOptions.identifyCount Then Exit For
-        Next
         labels(2) = CStr(redCells.Count) + " objects were found in the top view."
+    End Sub
+End Class
+
+
+
+
+
+
+
+Public Class HeatMap_Top : Inherits VB_Algorithm
+    Dim histTop As New Histogram2D_Top
+    Dim redC As New RedCloud_BasicsMask
+    Dim redCells As New List(Of rcData)
+    Public Sub New()
+        redOptions.ProjectionThreshold.Value = 1
+        desc = "Find all the masks, rects, and counts in the top down view."
+    End Sub
+    Public Sub RunVB(src As cv.Mat)
+        If task.heartBeat Then
+            Dim depthCount = task.pcSplit(2).CountNonZero
+            histTop.Run(src)
+            dst3 = histTop.dst2
+
+            redC.inputMask = Not histTop.dst3
+            redC.Run(histTop.dst3)
+
+            For i = 0 To redC.redCells.Count - 1
+                Dim rc = redC.redCells(i)
+                Dim tmp = New cv.Mat(rc.rect.Size, cv.MatType.CV_32F, 0)
+                dst3(rc.rect).CopyTo(tmp, rc.mask)
+                redC.redCells(i).pixels = tmp.Sum()
+            Next
+
+            For i = 0 To redC.redCells.Count - 1
+                Dim rcBig = redC.redCells(i)
+                For j = i + 1 To redC.redCells.Count - 1
+                    Dim rc = redC.redCells(j)
+                    If rc.pixels = 0 Then Continue For
+                    If rcBig.rect.Contains(rc.rect) Then
+                        rcBig.rect = rcBig.rect.Union(rc.rect)
+                        redC.redCells(i).pixels += rc.pixels
+                        redC.redCells(j).pixels = 0
+                    End If
+                Next
+            Next
+
+            Dim check1 = dst3.Sum()(0)
+
+            Dim sortedCells As New SortedList(Of Integer, rcData)(New compareAllowIdenticalIntegerInverted)
+            Dim check2 As Integer
+            For Each rc In redC.redCells
+                sortedCells.Add(rc.pixels, rc)
+                check2 += rc.pixels
+            Next
+
+            redCells.Clear()
+            redCells.Add(New rcData)
+            For Each rc In sortedCells.Values
+                rc.index = redCells.Count
+                redCells.Add(rc)
+            Next
+
+            Dim otherCount As Integer
+            strOut = ""
+            For Each rc In redCells
+                If rc.index = 0 Then Continue For
+                If rc.index <= redOptions.identifyCount Then
+                    dst2.Rectangle(rc.rect, task.highlightColor, task.lineWidth)
+                    Dim xy1 = (task.rangesSide(0).End - task.rangesSide(0).Start) * rc.rect.Y / dst2.Height
+                    Dim xy2 = (task.rangesSide(0).End - task.rangesSide(0).Start) * (rc.rect.Y + rc.rect.Height) / dst2.Height
+                    Dim z1 = (task.rangesSide(1).End - task.rangesSide(1).Start) * rc.rect.X / dst2.Width
+                    Dim z2 = (task.rangesSide(1).End - task.rangesSide(1).Start) * (rc.rect.X + rc.rect.Width) / dst2.Width
+                    strOut += "Object " + vbTab + CStr(rc.index) + vbTab + Format(xy2 - xy1, fmt3) + " m " + vbTab +
+                               Format(z1, fmt1) + "m to " + Format(z2, fmt1) + "m from camera" + vbTab + CStr(rc.pixels) + " pixels" + vbCrLf
+                Else
+                    otherCount += rc.pixels
+                End If
+            Next
+
+            strOut += "Other  " + vbTab + CStr(otherCount) + " pixels" + vbCrLf
+            strOut += "Total  " + vbTab + CStr(depthCount) + " pixels" + vbCrLf
+            strOut += "Check1 " + vbTab + CStr(depthCount) + " pixels" + vbCrLf
+            strOut += "Check2 " + vbTab + CStr(depthCount) + " pixels" + vbCrLf
+        End If
+        setTrueText(strOut, 3)
+
+        dst2 = redC.dst2
+        labels(2) = CStr(redCells.Count) + " objects were found in the top view."
+    End Sub
+End Class
+
+
+
+
+
+
+
+
+
+Public Class HeatMap_ObjectList : Inherits VB_Algorithm
+    Public redCellInput As New List(Of rcData)
+    Public redCells As New List(Of rcData)
+    Public viewType As String = "Top"
+    Public Sub New()
+        redOptions.ProjectionThreshold.Value = 1
+        desc = "Find all the masks, rects, and counts in the top down view."
+    End Sub
+    Public Sub RunVB(src As cv.Mat)
+        If task.heartBeat Then
+            If standalone Then
+                Static histTop As New Histogram2D_Top
+                histTop.Run(src)
+                src = histTop.dst2
+
+                Static redc As New RedCloud_BasicsMask
+                redc.inputMask = Not histTop.dst3
+                redC.Run(histTop.dst3)
+                redCellInput = redc.redCells
+                dst2 = redc.dst2
+                labels(2) = redc.labels(2)
+            End If
+            Dim depthCount = task.pcSplit(2).CountNonZero
+
+            For i = 0 To redCellInput.Count - 1
+                Dim rc = redCellInput(i)
+                Dim tmp = New cv.Mat(rc.rect.Size, cv.MatType.CV_32F, 0)
+                src(rc.rect).CopyTo(tmp, rc.mask)
+                redCellInput(i).pixels = tmp.Sum()
+            Next
+
+            For i = 0 To redCellInput.Count - 1
+                Dim rcBig = redCellInput(i)
+                For j = i + 1 To redCellInput.Count - 1
+                    Dim rc = redCellInput(j)
+                    If rc.pixels = 0 Then Continue For
+                    If rcBig.rect.Contains(rc.rect) Then
+                        rcBig.rect = rcBig.rect.Union(rc.rect)
+                        redCellInput(i).pixels += rc.pixels
+                        redCellInput(j).pixels = 0
+                    End If
+                Next
+            Next
+
+            Dim check1 = src.Sum()(0)
+
+            Dim sortedCells As New SortedList(Of Integer, rcData)(New compareAllowIdenticalIntegerInverted)
+            Dim check2 As Integer
+            For Each rc In redCellInput
+                sortedCells.Add(rc.pixels, rc)
+                check2 += rc.pixels
+            Next
+
+            redCells.Clear()
+            redCells.Add(New rcData)
+            For Each rc In sortedCells.Values
+                rc.index = redCells.Count
+                redCells.Add(rc)
+            Next
+
+            Dim otherCount As Integer
+            strOut = ""
+            Dim meterDesc = "high"
+            If viewType = "Top" Then meterDesc = "wide"
+            For Each rc In redCells
+                If rc.index = 0 Then Continue For
+                If rc.index <= redOptions.identifyCount Then
+                    dst2.Rectangle(rc.rect, task.highlightColor, task.lineWidth)
+                    Dim xy1 = (task.rangesSide(0).End - task.rangesSide(0).Start) * rc.rect.Y / dst2.Height
+                    Dim xy2 = (task.rangesSide(0).End - task.rangesSide(0).Start) * (rc.rect.Y + rc.rect.Height) / dst2.Height
+                    Dim z1 = (task.rangesSide(1).End - task.rangesSide(1).Start) * rc.rect.X / dst2.Width
+                    Dim z2 = (task.rangesSide(1).End - task.rangesSide(1).Start) * (rc.rect.X + rc.rect.Width) / dst2.Width
+                    strOut += "Object " + vbTab + CStr(rc.index) + vbTab + Format(xy2 - xy1, fmt3) + " m " + meterDesc + vbTab +
+                               Format(z1, fmt1) + "m " + " to " + Format(z2, fmt1) + "m from camera" + vbTab + CStr(rc.pixels) + " pixels" + vbCrLf
+                Else
+                    otherCount += rc.pixels
+                End If
+            Next
+
+            strOut += "Other  " + vbTab + CStr(otherCount) + " pixels" + vbCrLf
+            strOut += "Total  " + vbTab + CStr(depthCount) + " pixels" + vbCrLf
+            strOut += "Check1 " + vbTab + CStr(depthCount) + " pixels" + vbCrLf
+            strOut += "Check2 " + vbTab + CStr(depthCount) + " pixels" + vbCrLf
+        End If
+        setTrueText(strOut, 3)
+
+        labels(2) = CStr(redCells.Count) + " objects were found in the " + viewType + " view."
     End Sub
 End Class
