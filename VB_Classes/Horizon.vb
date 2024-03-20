3c6abba8f8962b1a9b9a3df13b87a558122d3386
Bob Davies <bobdavies2000@gmail.com>
2024-03-19 20:57:25 -0700

    Finished Horizon_basics

--- VB_Classes/Horizon.vb
+++ VB_Classes/Horizon.vb
@@ -2,52 +2,49 @@
 Public Class Horizon_Basics : Inherits VB_Algorithm
     Dim lines As New Line_Basics
     Public Sub New()
-        redOptions.YRangeSlider.Value = 30
-        desc = "Create a mask of the surv"
+        dst2 = New cv.Mat(dst2.Size, cv.MatType.CV_8U, 0)
+        desc = "Create a mask of the vertical and horizontal orientation of the camera"
     End Sub
     Public Sub RunVB(src As cv.Mat)
         If src.Type <> cv.MatType.CV_32FC3 Then src = task.pointCloud
 
-        dst2.SetTo(0)
         dst1 = task.pcSplit(1).InRange(-0.05, 0.05)
-        dst2.SetTo(cv.Scalar.White, dst1)
+        dst0.SetTo(0)
+        dst0.SetTo(cv.Scalar.White, dst1)
+        dst0.SetTo(0, task.noDepthMask)
+        lines.Run(dst0)
+
+        dst2.SetTo(0)
+        If lines.lpList.Count > 0 Then
+            Dim distances As New SortedList(Of Single, pointPair)(New compareAllowIdenticalSingleInverted)
+            For Each lp In lines.lpList
+                distances.Add(lp.p1.DistanceTo(lp.p2), lp)
+            Next
+
+            Dim lpBest = distances.ElementAt(0).Value
+            Dim p1 = New cv.Point2f(0, lpBest.yIntercept)
+            Dim p2 = New cv.Point2f(dst2.Width, lpBest.slope * dst2.Width + lpBest.yIntercept)
+            dst2.Line(p1, p2, cv.Scalar.White, task.lineWidth, task.lineType)
+            labels(2) = "Horizontal slope/intercept = " + Format(lpBest.slope, fmt1) + "/" + Format(lpBest.yIntercept, fmt1)
+        End If
 
         dst1 = task.pcSplit(0).InRange(-0.01, 0.01)
-        dst2.SetTo(cv.Scalar.White, dst1)
+        dst0.SetTo(0)
+        dst0.SetTo(cv.Scalar.White, dst1)
+        dst0.SetTo(0, task.noDepthMask)
+        lines.Run(dst0)
 
-        dst2.SetTo(0, task.noDepthMask)
+        If lines.lpList.Count > 0 Then
+            Dim distances As New SortedList(Of Single, pointPair)(New compareAllowIdenticalSingleInverted)
+            For Each lp In lines.lpList
+                distances.Add(lp.p1.DistanceTo(lp.p2), lp)
+            Next
 
-        lines.Run(dst2)
-        Dim hSlope As New List(Of Single)
-        Dim vSlope As New List(Of Single)
-        Dim hCept As New List(Of Single)
-        Dim vCept As New List(Of Single)
-        For Each lp In lines.lpList
-            If lp.yIntercept > 0 Then
-                hSlope.Add(lp.slope)
-                hCept.Add(lp.yIntercept)
-            Else
-                vSlope.Add(lp.slope)
-                vCept.Add(lp.yIntercept)
-            End If
-        Next
-
-        Dim horizonSlope = hSlope.Average()
-        Dim horizonCept = hCept.Average()
-
-        Dim vertSlope = vSlope.Average()
-        Dim vertCept = vCept.Average()
-
-        dst3.SetTo(0)
-        Dim p1 = New cv.Point2f(0, horizonCept)
-        Dim p2 = New cv.Point2f(dst2.Width, horizonSlope * dst2.Width + horizonCept)
-        dst3.Line(p1, p2, task.highlightColor, task.lineWidth, task.lineType)
-
-        Dim p3 = New cv.Point2f(0, vertCept)
-        Dim p4 = New cv.Point2f(dst2.Width, vertSlope * dst2.Width + vertCept)
-        dst3.Line(p3, p4, task.highlightColor, task.lineWidth, task.lineType)
-
-        labels(2) = "horizon slope/intercept = " + Format(horizonSlope, fmt1) + "/" + Format(horizonCept, fmt1)
-        labels(3) = "vertical slope/intercept = " + Format(vertSlope, fmt1) + "/" + Format(vertCept, fmt1)
+            Dim lpBest = distances.ElementAt(0).Value
+            Dim p1 = New cv.Point2f(0, lpBest.yIntercept)
+            Dim p2 = New cv.Point2f(dst2.Width, lpBest.slope * dst2.Width + lpBest.yIntercept)
+            dst2.Line(p1, p2, cv.Scalar.White, task.lineWidth, task.lineType)
+            labels(3) = "Vertical slope/intercept = " + Format(lpBest.slope, fmt1) + "/" + Format(lpBest.yIntercept, fmt1)
+        End If
     End Sub
 End Class
