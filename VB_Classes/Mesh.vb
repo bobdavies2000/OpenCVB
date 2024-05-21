081eedfa7c5face13d08c2f610fa5d67e87a772e
Bob Davies <bobdavies2000@gmail.com>
2024-05-21 11:14:48 -0700

    Improved Mesh_Basics - more general.

--- VB_Classes/Mesh.vb
+++ VB_Classes/Mesh.vb
@@ -2,11 +2,16 @@
 Public Class Mesh_Basics : Inherits VB_Algorithm
     Dim knn As New KNN_Core
     Public Sub New()
+        If sliders.Setup(traceName) Then sliders.setupTrackBar("Number of nearest neighbors", 0, 10, 2)
+
         labels(2) = "Triangles built with each random point and its 2 nearest neighbors."
         vbAddAdvice(traceName + ": Adjust the number of points with the options_random.")
         desc = "Build triangles from random points"
     End Sub
     Public Function showMesh(pointList As List(Of cv.Point2f)) As cv.Mat
+        Static nabeSlider = findSlider("Number of nearest neighbors")
+        Dim nabeCount = nabeSlider.value
+
         If pointList.Count <= 3 Then Return dst2 ' Not enough points To draw...
 
         knn.queries = pointList
@@ -14,15 +19,17 @@ Public Class Mesh_Basics : Inherits VB_Algorithm
         knn.Run(empty)
 
         For i = 0 To knn.queries.Count - 1
-            Dim p0 = knn.queries(i)
-            Dim p1 = knn.queries(knn.result(i, 1))
-            Dim p2 = knn.queries(knn.result(i, 2))
-            dst2.Line(p0, p1, white, task.lineWidth, task.lineType)
-            dst2.Line(p0, p2, white, task.lineWidth, task.lineType)
-            dst2.Line(p1, p2, white, task.lineWidth, task.lineType)
+            Dim ptLast = knn.queries(i)
+            For j = 1 To nabeCount - 1
+                Dim pt = knn.queries(knn.result(i, j))
+                dst2.Line(ptLast, pt, white, task.lineWidth, task.lineType)
+                ptLast = pt
+            Next
         Next
+        dst3.SetTo(0)
         For i = 0 To knn.queries.Count - 1
             dst2.Circle(knn.queries(i), task.dotSize, cv.Scalar.Red, -1, task.lineType)
+            dst3.Circle(knn.queries(i), task.dotSize, task.highlightColor, -1, task.lineType)
         Next
         Return dst2
     End Function
@@ -47,7 +54,8 @@ Public Class Mesh_Features : Inherits VB_Algorithm
     Dim feat As New Feature_Basics
     Dim mesh As New Mesh_Basics
     Public Sub New()
-        labels(2) = "Triangles built with each feature point and its 2 nearest neighbors."
+        findSlider("Min Distance to next").Value = 10
+        labels(2) = "Triangles built with each feature point and the specified number of nearest neighbors."
         vbAddAdvice(traceName + ": Use 'Options_Features' to update results.")
         desc = "Build triangles from feature points"
     End Sub
@@ -56,27 +64,6 @@ Public Class Mesh_Features : Inherits VB_Algorithm
         If task.features.Count < 3 Then Exit Sub
         mesh.dst2 = src
         dst2 = mesh.showMesh(task.features)
-    End Sub
-End Class
-
-
-
-
-
-
-Public Class Mesh_Agast : Inherits VB_Algorithm
-    Dim gather As New Feature_Gather
-    Dim mesh As New Mesh_Basics
-    Public Sub New()
-        findRadio("Agast Features").Checked = True
-        labels(2) = "Triangles built with each feature point and its 2 nearest neighbors."
-        desc = "Build triangles from Agast points"
-    End Sub
-    Public Sub RunVB(src As cv.Mat)
-        gather.Run(src)
-        If gather.features.Count < 3 Then Exit Sub
-        mesh.dst2 = src
-        dst2 = mesh.showMesh(gather.features)
-        labels(3) = gather.labels(2)
+        dst3 = mesh.dst3
     End Sub
 End Class
