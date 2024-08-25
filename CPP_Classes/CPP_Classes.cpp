c9b71b094b2a3784c23c977c3a99f8c5c6ba2070
Bob Davies <bobdavies2000@gmail.com>
2024-08-25 09:37:27 -0700

    Using SolutionDir - simpler.

--- CPP_Classes/CPP_Classes.cpp
+++ CPP_Classes/CPP_Classes.cpp
@@ -1,2 +1,52 @@
 #include "CPP_Classes.h"
+#include <string.h>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <opencv2/core.hpp>
+#include <opencv2/ximgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include "opencv2/core/utility.hpp"
+#include "opencv2/ml/ml.hpp "
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include <numeric>
+#include <iomanip>
+#include <sstream>
+#include <memory>
+#include <vector>
+#include <random>
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/bgsegm.hpp"
+#include "opencv2/photo.hpp"
+#include <map>
+#include <opencv2/ml.hpp>
+#include <opencv2/plot.hpp>
+#include "opencv2/ccalib/randpattern.hpp"
+#include "opencv2/xphoto/oilpainting.hpp"
+
+using namespace System;
+using namespace cv;
+
+namespace CPP_Classes {
+    class AddWeighted_Basics_CC
+    {
+    public:
+        double weight;
+        Mat src2;
+        Mat dst2;
+        AddWeighted_Basics_CC()
+        {
+            auto desc = "Add 2 images with specified weights.";
+        }
+
+        void RunCS(cv::Mat& src)
+        {
+            Mat srcPlus = src2;
+            double weight = 0.5;
+            addWeighted(src, weight, srcPlus, 1.0 - weight, 0, dst2);
+        }
+    };
+}
 
--- CPP_Classes/CPP_Classes.h
+++ CPP_Classes/CPP_Classes.h
@@ -1,10 +1,2 @@
 #pragma once
 
-using namespace System;
-
-namespace CPP_Classes {
-	public ref class Class1
-	{
-		// TODO: Add your methods for this class here.
-	};
-}
