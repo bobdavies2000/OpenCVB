fc610f8f286b8ea64118a1ebfabd4c2ccd72faef
Bob Davies <bobdavies2000@gmail.com>
2024-09-16 10:44:24 -0700

    StereoLabs default is too cumbersome for most - requires CUDA and NVIDIA.

--- Cameras/CameraDefines.hpp
+++ Cameras/CameraDefines.hpp
@@ -1,3 +1,3 @@
-#define STEREOLAB_INSTALLED // support for Zed 2 camera 
+//#define STEREOLAB_INSTALLED // support for Zed 2 camera 
 //#define MYNTD_1000 // support for MyntD cameras
 #define OAKD // support for Oak-D
