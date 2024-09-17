cc5dc5ebbdd1deb8fb04040a68750e577d1b96b5
Bob Davies <bobdavies2000@gmail.com>
2024-09-17 09:47:54 -0700

    Testing for CUDA in install script.

--- test.bat
+++ test.bat
@@ -7,8 +7,10 @@ if errorlevel 1 goto errorNoPython
 :: Reaching here means Python is installed.
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
-if not exist zed-c-api\Build and exist "c:\Program Files\NVIDIA GPU Computing Toolkit\CUDA" (
-	"C:\Program Files\CMake\bin\Cmake.exe" -S zed-c-api -B zed-c-api/Build -DCMAKE_CONFIGURATION_TYPES=Debug;Release
-	msbuild.exe zed-c-api/Build/C.sln /p:Configuration=Debug
-	msbuild.exe zed-c-api/Build/C.sln /p:Configuration=Release
+if not exist zed-c-api\Build (
+	if exist "c:\Program Files\NVIDIA GPU Computing Toolkit\CUDA" (
+		"C:\Program Files\CMake\bin\Cmake.exe" -S zed-c-api -B zed-c-api/Build -DCMAKE_CONFIGURATION_TYPES=Debug;Release
+		msbuild.exe zed-c-api/Build/C.sln /p:Configuration=Debug
+		msbuild.exe zed-c-api/Build/C.sln /p:Configuration=Release
+	)
 )
