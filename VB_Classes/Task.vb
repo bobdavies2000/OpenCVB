2a7798af889eb341f2c2d26d7b3ef8b48f2abfe8
Bob Davies <bobdavies2000@gmail.com>
2025-01-09 20:00:11 -0800

    Test all has no treeview so no mark for inactive tasks.

--- VB_Classes/Task.vb
+++ VB_Classes/Task.vb
@@ -867,11 +867,13 @@ Public Class VBtask : Implements IDisposable
 
             ' if there were no cycles spent on this routine, then it was inactive.
             ' if any active algorithm has an index = -1, make sure it is running .Run, not .runAlg
-            Dim index = algorithmNames.IndexOf(displayObject.traceName)
-            If index = -1 Then
-                Dim str As New TrueText("This task is not active at this time.",
+            If task.testAllRunning = False Then ' no Treeview when running test all...
+                Dim index = algorithmNames.IndexOf(displayObject.traceName)
+                If index = -1 Then
+                    Dim str As New TrueText("This task is not active at this time.",
                                         New cv.Point(dst2.Width / 2, 0), 3)
-                displayObject.trueData.Add(str)
+                    displayObject.trueData.Add(str)
+                End If
             End If
 
             trueData = New List(Of TrueText)(displayObject.trueData)
