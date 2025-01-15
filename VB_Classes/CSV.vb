bb81125717361812d03ea6569f2b07c16689f1bc
Bob Davies <bobdavies2000@gmail.com>
2025-01-15 08:43:57 -0800

    Memory overconsumption problem in CSV_Basics.

--- VB_Classes/CSV.vb
+++ VB_Classes/CSV.vb
@@ -5,15 +5,17 @@ Public Class CSV_Basics : Inherits TaskParent
     Public inputFile As String
     Public array(,) As String
     Public arrayList As New List(Of List(Of String))
+    Dim readText() As String
+    Dim variables() As String
     Public Sub New()
         Dim fileInput As New FileInfo(task.HomeDir + "Data/agaricus-lepiota.data")
         inputFile = fileInput.FullName
+        readText = File.ReadAllLines(inputFile) ' user supplies the inputfile name.
+        Dim variables = readText(0).Split(",")
+        ReDim array(readText.Length - 1, variables.Length - 1)
         desc = "Read and prepare a .csv file"
     End Sub
     Public Overrides sub RunAlg(src As cv.Mat)
-        Dim readText() As String = File.ReadAllLines(inputFile) ' user supplies the inputfile name.
-        Dim variables = readText(0).Split(",")
-        ReDim array(readText.Length - 1, variables.Length - 1)
         For i = 0 To array.GetUpperBound(0)
             variables = readText(i).Split(",")
             For j = 0 To array.GetUpperBound(1)
@@ -28,5 +30,7 @@ Public Class CSV_Basics : Inherits TaskParent
             Next
         Next
         If standaloneTest() Then SetTrueText(inputFile + " is now loaded into the csv.array")
+
+        ReDim array(0, 0)
     End Sub
 End Class
