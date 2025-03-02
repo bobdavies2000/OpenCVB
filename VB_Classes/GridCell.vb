5cf67d71ae5422096b4b0049cba1bbbf90847a05
Bob Davies <bobdavies2000@gmail.com>
2025-03-01 19:27:57 -0800

    GridCell_Plot update.

--- VB_Classes/GridCell.vb
+++ VB_Classes/GridCell.vb
@@ -173,7 +173,7 @@ Public Class GridCell_Plot : Inherits TaskParent
             dst3.SetTo(0)
             Exit Sub
         End If
-        Dim split() = task.pointCloud(idd.lRect).Split()
+        Dim split() = task.pointCloud(idd.cRect).Split()
         Dim mm = GetMinMax(split(2))
 
         If Math.Abs(mm.maxVal - mm.minVal) > 0 Then
