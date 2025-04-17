806703f9914aee6ac6daa0364b8d5e066a8f34dc
Bob Davies <bobdavies2000@gmail.com>
2025-04-17 06:36:06 -0700

    Improved lineRect_Basics.

--- VB_Classes/LineRect.vb
+++ VB_Classes/LineRect.vb
@@ -8,37 +8,26 @@ Public Class LineRect_Basics : Inherits TaskParent
         desc = "Create a rectangle from 2 lines"
     End Sub
     Public Overrides Sub RunAlg(src As cv.Mat)
-        Dim lp1 = lpInput1, lp2 = lpInput2
-        If lp1 Is Nothing Then
+        If standalone Then
             Dim p1 = New cv.Point2f(msRNG.Next(0, dst2.Width), msRNG.Next(0, dst2.Height))
             Dim p2 = New cv.Point2f(msRNG.Next(0, dst2.Width), msRNG.Next(0, dst2.Height))
             Dim p3 = New cv.Point2f(msRNG.Next(0, dst2.Width), msRNG.Next(0, dst2.Height))
             Dim p4 = New cv.Point2f(msRNG.Next(0, dst2.Width), msRNG.Next(0, dst2.Height))
-            lp1 = New lpData(p1, p2)
-            lp2 = New lpData(p3, p4)
+            lpInput1 = New lpData(p1, p2)
+            lpInput2 = New lpData(p3, p4)
         End If
 
-        Dim inputPoints As New List(Of cv.Point2f)
-        inputPoints.Add(lp1.p1)
-        inputPoints.Add(lp1.p2)
-        inputPoints.Add(lp2.p1)
-        inputPoints.Add(lp2.p2)
-        If lp1.rotatedRect = Nothing And lp2.rotatedRect = Nothing Then
-            rotatedRect = cv.Cv2.MinAreaRect(inputPoints.ToArray)
-        ElseIf lp1.rotatedRect = Nothing Then
-            For Each pt In lp2.rotatedRect.Points
-                inputPoints.Add(pt)
-            Next
-        Else
-            For Each pt In lp1.rotatedRect.Points
-                inputPoints.Add(pt)
-            Next
-        End If
+        Dim inputPoints() As cv.Point2f = {lpInput1.p1, lpInput1.p2, lpInput2.p1, lpInput2.p2}
+        rotatedRect = cv.Cv2.MinAreaRect(inputPoints)
         If standalone And task.heartBeat Then
             dst2.SetTo(0)
             For Each pt In inputPoints
                 DrawCircle(dst2, pt, task.DotSize, task.highlight)
             Next
+            dst2.Line(lpInput1.p1, lpInput1.p2, task.highlight, task.lineWidth, task.lineType)
+            dst2.Line(lpInput2.p1, lpInput2.p2, task.highlight, task.lineWidth, task.lineType)
+            SetTrueText("Line 1", lpInput1.p1, 2)
+            SetTrueText("Line 2", lpInput2.p1, 2)
             DrawRotatedOutline(rotatedRect, dst2, cv.Scalar.Yellow)
         End If
     End Sub
