117478db9dcce6ceda47c07a2ca2df35b93ff078
Bob Davies <bobdavies2000@gmail.com>
2025-08-13 07:16:35 -0700

    Initial GLSharp

---
+++ VBClasses/GLSharp.Designer.vb
@@ -0,0 +1,36 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GLSharp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        SuspendLayout()
+        ' 
+        ' GLSharp
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Name = "GLSharp"
+        Text = "GLSharp"
+        ResumeLayout(False)
+    End Sub
+End Class
---
+++ VBClasses/GLSharp.vb
@@ -0,0 +1,6 @@
+﻿'Imports SharpGL
+'Public Class GLSharp
+'    Private Sub GLSharp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+'        Dim gl As OpenGL = OpenGLControl.OpenGL
+'    End Sub
+'End Class
