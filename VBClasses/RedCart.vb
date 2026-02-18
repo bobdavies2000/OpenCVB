9bccd020a9cde9fdc112b8ef46b2d50ec132f0d3
Bob Davies <bobdavies2000@gmail.com>
2026-02-17 18:33:42 -0800

    RedWC added and validation working.

--- VBClasses/RedCart.vb
+++ VBClasses/RedCart.vb
@@ -10,33 +10,9 @@ Namespace VBClasses
             OptionParent.findRadio("X Reduction").Checked = True
             desc = "Run RedCloud on the output of RedPrep_Core"
         End Sub
-        Public Overrides Sub RunAlg(src As cv.Mat)
-            prepData.Run(emptyMat)
-            redC.Run(src)
-            dst2 = redC.dst2
-            labels(2) = redC.labels(2)
-        End Sub
-    End Class
-
-
-
-
-
-    Public Class RedCart_Basics1 : Inherits TaskParent
-        Public prepData As New RedPrep_Core
-        Public lut As New cv.Mat
-        Public lutList As New List(Of Byte)
-        Public Sub New()
-            task.gOptions.DebugSlider.Value = 1
-            labels(3) = "Use debug slider to select region to display."
-            OptionParent.findRadio("X Reduction").Checked = True
-            desc = "Prepare the grid of point cloud data."
-        End Sub
         Public Shared Function countClasses(input As cv.Mat, ByRef label As String) As cv.Mat
             Dim histogram As New cv.Mat
-            Dim mm = GetMinMax(input)
-            Dim ranges = {New cv.Rangef(mm.minVal, mm.maxVal)}
-            cv.Cv2.CalcHist({input}, {0}, task.depthmask, histogram, 1, {255}, ranges)
+            cv.Cv2.CalcHist({input}, {0}, task.depthmask, histogram, 1, {256}, {New cv.Rangef(0, 256)})
             Dim histArray(255) As Single
             Marshal.Copy(histogram.Data, histArray, 0, histArray.Length)
 
@@ -53,14 +29,36 @@ Namespace VBClasses
             Dim lut As New cv.Mat(1, 256, cv.MatType.CV_8U)
             lut.SetArray(Of Byte)(lutArray)
 
-            label = CStr(regionList.Count) + " non-zero regions."
+            label = CStr(regionList.Count) + " non-zero regions more than " + CStr(sizeThreshold) + " pixels"
             Return lut
         End Function
         Public Overrides Sub RunAlg(src As cv.Mat)
             prepData.Run(emptyMat)
+            redC.Run(src)
+            dst2 = redC.dst2
+            labels(2) = redC.labels(2)
+        End Sub
+    End Class
+
+
+
+
+
+    Public Class NR_RedCart_Basics : Inherits TaskParent
+        Public prepData As New RedPrep_Core
+        Public lut As New cv.Mat
+        Public lutList As New List(Of Byte)
+        Public Sub New()
+            task.gOptions.DebugSlider.Value = 1
+            labels(3) = "Use debug slider to select region to display."
+            OptionParent.findRadio("X Reduction").Checked = True
+            desc = "Prepare the grid of point cloud data."
+        End Sub
+        Public Overrides Sub RunAlg(src As cv.Mat)
+            prepData.Run(emptyMat)
             labels(2) = prepData.labels(2)
 
-            lut = countClasses(prepData.dst2.Clone, labels(2))
+            lut = RedCart_Basics.countClasses(prepData.dst2.Clone, labels(2))
             lutList.Clear()
             For i = 0 To lut.Cols - 1
                 Dim val = lut.Get(Of Byte)(0, i)
@@ -82,39 +80,6 @@ Namespace VBClasses
 
 
 
-    Public Class RedCart_Debug : Inherits TaskParent
-        Dim redCart As New RedCart_Basics1
-        Public classCount As Integer
-        Public Sub New()
-            If standalone Then task.gOptions.displayDst1.Checked = True
-            desc = "Identify each region using the debug slider."
-        End Sub
-        Public Overrides Sub RunAlg(src As cv.Mat)
-            redCart.Run(emptyMat)
-            dst3 = redCart.dst2
-            labels(3) = redCart.labels(2)
-            strOut = ""
-
-            For i = 1 To redCart.lutList.Count - 1
-                dst2 = redCart.dst1.InRange(i, i)
-                Dim mean = redCart.prepData.reduced32s.Mean(dst2)
-                strOut += "Mean of selected region " + CStr(i) + " = " + Format(mean(0), fmt0) + "  "
-                If i Mod 2 = 0 Then strOut += vbCrLf
-            Next
-
-            dst2 = (redCart.prepData.reduced32s - -1000) * 255 / (2000)
-            dst2.ConvertTo(dst2, cv.MatType.CV_8U)
-            dst2.SetTo(0, task.noDepthMask)
-
-
-            labels(1) = CStr(redCart.lutList.Count) + " non-zero regions."
-            SetTrueText(strOut, 1)
-        End Sub
-    End Class
-
-
-
-
     Public Class RedCart_PrepXY : Inherits TaskParent
         Public redCart As New RedCart_Basics
         Public Sub New()
@@ -131,7 +96,7 @@ Namespace VBClasses
 
 
 
-    Public Class RedCart_PrepData : Inherits TaskParent
+    Public Class NR_RedCart_PrepData : Inherits TaskParent
         Dim prepData As New RedPrep_Core
         Public Sub New()
             desc = "Prepare the grid of point cloud data."
@@ -149,8 +114,8 @@ Namespace VBClasses
 
 
 
-    Public Class RedCart_Validate : Inherits TaskParent
-        Dim redCart As New RedCart_PrepY
+    Public Class NR_RedCart_Validate : Inherits TaskParent
+        Dim redCart As New NR_RedCart_PrepY
         Public Sub New()
             dst1 = New cv.Mat(dst1.Size, cv.MatType.CV_8U, 0)
             task.gOptions.displayDst1.Checked = True
@@ -184,7 +149,7 @@ Namespace VBClasses
 
 
 
-    Public Class RedCart_CheckerBoardWall : Inherits TaskParent
+    Public Class NR_RedCart_CheckerBoardWall : Inherits TaskParent
         Public prepData As New RedPrep_Core
         Public classCount As Integer
         Dim edges As New Edge_Basics
@@ -195,7 +160,7 @@ Namespace VBClasses
             ' prepData.reductionName = "XY Reduction" ' default
             prepData.Run(src)
 
-            Dim lut = RedCart_Basics1.countClasses(prepData.dst2, labels(2))
+            Dim lut = RedCart_Basics.countClasses(prepData.dst2, labels(2))
             dst2 = prepData.dst2.LUT(lut)
 
             edges.Run(dst2)
@@ -206,31 +171,8 @@ Namespace VBClasses
 
 
 
-    Public Class RedCart_TriangleDots : Inherits TaskParent
-        Dim checkers As New RedPrep_Core
-        Public Sub New()
-            desc = "Find any "
-        End Sub
-        Public Overrides Sub RunAlg(src As cv.Mat)
-            checkers.Run(src)
-            Dim kernel = Cv2.GetStructuringElement(MorphShapes.Rect, New Size(3, 3))
 
-            cv.Cv2.Erode(checkers.dst2, dst2, kernel)
-
-            Dim mask As New Mat(dst2.Rows + 2, dst2.Cols + 2, MatType.CV_8UC1)
-            mask.SetTo(0)
-
-            ' Cv2.FloodFill(dst2, mask, seedPoint, New Scalar(255))
-
-
-        End Sub
-    End Class
-
-
-
-
-
-    Public Class RedCart_CPP : Inherits TaskParent
+    Public Class NR_RedCart_CPP : Inherits TaskParent
         Implements IDisposable
         Dim prep As New RedPrep_Core
         Public Sub New()
@@ -260,7 +202,7 @@ Namespace VBClasses
 
 
 
-    Public Class RedCart_PrepXOld : Inherits TaskParent
+    Public Class NR_RedCart_PrepXOld : Inherits TaskParent
         Public prepData As New RedPrep_Core
         Public classCount As Integer
         Public lut As cv.Mat
@@ -272,14 +214,14 @@ Namespace VBClasses
             prepData.Run(src)
             dst2 = prepData.dst3
 
-            Dim lut = RedCart_Basics1.countClasses(prepData.dst2, labels(2))
+            Dim lut = RedCart_Basics.countClasses(prepData.dst2, labels(2))
             dst2 = prepData.dst2.LUT(lut)
         End Sub
     End Class
 
 
 
-    Public Class RedCart_PrepY : Inherits TaskParent
+    Public Class NR_RedCart_PrepY : Inherits TaskParent
         Public prepData As New RedPrep_Core
         Public classCount As Integer
         Public Sub New()
@@ -290,7 +232,7 @@ Namespace VBClasses
             prepData.Run(src)
             dst2 = prepData.dst3
 
-            Dim lut = RedCart_Basics1.countClasses(prepData.dst2, labels(2))
+            Dim lut = RedCart_Basics.countClasses(prepData.dst2, labels(2))
             dst2 = prepData.dst2.LUT(lut)
         End Sub
     End Class
@@ -299,8 +241,8 @@ Namespace VBClasses
 
 
     Public Class RedCart_PrepXYAlt : Inherits TaskParent
-        Dim redX As New RedCart_PrepX
-        Dim redY As New RedCart_PrepY
+        Dim redX As New NR_RedCart_PrepX
+        Dim redY As New NR_RedCart_PrepY
         Public Sub New()
             dst1 = New cv.Mat(dst1.Size, cv.MatType.CV_8U, 0)
             dst2 = New cv.Mat(dst2.Size, cv.MatType.CV_8U, 0)
@@ -329,7 +271,7 @@ Namespace VBClasses
 
 
 
-    Public Class RedCart_PrepX : Inherits TaskParent
+    Public Class NR_RedCart_PrepX : Inherits TaskParent
         Public prepData As New RedPrep_Core
         Public classCount As Integer
         Public Sub New()
@@ -341,7 +283,7 @@ Namespace VBClasses
             prepData.Run(src)
             dst2 = prepData.dst3
 
-            Dim lut = RedCart_Basics1.countClasses(prepData.dst2, labels(2))
+            Dim lut = RedCart_Basics.countClasses(prepData.dst2, labels(2))
             dst2 = prepData.dst2.LUT(lut)
         End Sub
     End Class
@@ -350,25 +292,34 @@ Namespace VBClasses
 
 
 
-    Public Class RedCart_PrepWC : Inherits TaskParent
-        Dim prepData As New RedPrep_Core
+    Public Class RedCart_Debug : Inherits TaskParent
+        Dim redCart As New NR_RedCart_Basics
+        Public classCount As Integer
         Public Sub New()
-            OptionParent.findRadio("X Reduction").Checked = True
-            desc = "Prepare the absolute coordinates of the World Coordinates."
+            If standalone Then task.gOptions.displayDst1.Checked = True
+            desc = "Identify each region using the debug slider."
         End Sub
         Public Overrides Sub RunAlg(src As cv.Mat)
-            Static radioX = OptionParent.findRadio("X Reduction")
-            Static radioY = OptionParent.findRadio("Y Reduction")
+            redCart.Run(emptyMat)
+            dst3 = redCart.dst2
+            labels(3) = redCart.labels(2)
+            strOut = ""
 
-            radioX.checked = True
-            prepData.Run(src)
-            dst2 = prepData.dst2
-            labels(2) = prepData.labels(2)
+            For i = 1 To redCart.lutList.Count - 1
+                dst2 = redCart.dst1.InRange(i, i)
+                Dim mean = redCart.prepData.reduced32s.Mean(dst2)
+                strOut += "Mean of selected region " + CStr(i) + " = " + Format(mean(0), fmt0) + "  "
+                If i Mod 2 = 0 Then strOut += vbCrLf
+            Next
 
-            radioY.checked = True
-            prepData.Run(src)
-            dst3 = prepData.dst2
-            labels(3) = prepData.labels(2)
+            dst2 = (redCart.prepData.reduced32s - -1000) * 255 / (2000)
+            dst2.ConvertTo(dst2, cv.MatType.CV_8U)
+            dst2.SetTo(0, task.noDepthMask)
+
+
+            labels(1) = CStr(redCart.lutList.Count) + " non-zero regions."
+            SetTrueText(strOut, 1)
         End Sub
     End Class
 End Namespace
+
---
+++ VBClasses/RedWC.vb
@@ -0,0 +1,172 @@
+﻿Imports System.Runtime.InteropServices
+Imports OpenCvSharp
+Imports VBClasses.Structures
+Imports cv = OpenCvSharp
+Namespace VBClasses
+    Public Class RedWC_Basics : Inherits TaskParent
+        Public prepData As New RedPrep_Core
+        Public Sub New()
+            If standalone Then OptionParent.findRadio("X Reduction").Checked = True
+            desc = "Prepare the absolute coordinates of the World Coordinates."
+        End Sub
+        Public Shared Function countRegions(input As cv.Mat, ByRef label As String) As List(Of Integer)
+            Dim histogram As New Mat
+            cv.Cv2.CalcHist({input}, {0}, task.depthmask, histogram, 1, {256}, {New cv.Rangef(0, 256)})
+            Dim histArray(255) As Single
+            Marshal.Copy(histogram.Data, histArray, 0, histArray.Length)
+
+            Dim sizeThreshold = input.Total * 0.001 ' ignore regions less than 0.1% - 1/10th of 1%
+            Dim lutArray(255) As Byte
+            Dim regionList As New List(Of Integer)
+            For i = 1 To histArray.Count - 1
+                If histArray(i) > sizeThreshold Then regionList.Add(i)
+            Next
+
+            label = CStr(regionList.Count) + " non-zero regions more than " + CStr(CInt(sizeThreshold)) + " pixels"
+            Return regionList
+        End Function
+        Public Overrides Sub RunAlg(src As cv.Mat)
+            prepData.Run(src)
+            dst2 = prepData.dst2
+            labels(2) = prepData.labels(2)
+        End Sub
+    End Class
+
+
+
+
+
+
+    Public Class RedWC_X : Inherits TaskParent
+        Public wcData As New RedWC_Basics
+        Public Sub New()
+            desc = "Assign world coordinates to each rcList entry."
+        End Sub
+        Public Overrides Sub RunAlg(src As cv.Mat)
+            wcData.Run(src)
+            dst2 = PaletteBlackZero(wcData.dst2)
+        End Sub
+    End Class
+
+
+
+
+
+    Public Class RedWC_Y : Inherits TaskParent
+        Public wcData As New RedWC_Basics
+        Public Sub New()
+            desc = "Assign world coordinates to each rcList entry."
+        End Sub
+        Public Overrides Sub RunAlg(src As cv.Mat)
+            wcData.Run(src)
+            dst2 = PaletteBlackZero(wcData.dst2)
+        End Sub
+    End Class
+
+
+
+
+
+
+
+    Public Class RedWC_Validate : Inherits TaskParent
+        Public wcData As New RedWC_Basics
+        Public Sub New()
+            If standalone Then OptionParent.findRadio("X Reduction").Checked = True
+            desc = "Identify each WC region."
+        End Sub
+        Public Overrides Sub RunAlg(src As cv.Mat)
+            wcData.Run(emptyMat)
+            dst2 = wcData.dst2
+
+            Dim regionlist = RedWC_Basics.countRegions(dst2, labels(3))
+            strOut = ""
+            Dim count As Integer
+            For i = 0 To regionlist.Count - 1
+                Dim index = regionlist(i)
+                dst0 = dst2.InRange(index, index)
+                Dim mean = wcData.prepData.reduced32s.Mean(dst0)
+
+                If CInt(mean(0)) Mod task.fOptions.ReductionSlider.Value = 0 Then
+                    Dim region = CInt(mean(0) / task.fOptions.ReductionSlider.Value)
+                    strOut += "region " + CStr(region) + " = " + Format(mean(0), fmt0) + "  "
+                    If i Mod 3 = 0 And i > 0 Then strOut += vbCrLf
+                    count += 1
+                End If
+            Next
+            SetTrueText(strOut, 3)
+            labels(2) = CStr(count) + " regions were found"
+        End Sub
+    End Class
+
+
+
+
+
+
+    Public Class RedWC_ValidateX : Inherits TaskParent
+        Dim redWC As New RedWC_X
+        Public Sub New()
+            If standalone Then OptionParent.findRadio("X Reduction").Checked = True
+            desc = "Identify each WC region."
+        End Sub
+        Public Overrides Sub RunAlg(src As cv.Mat)
+            redWC.Run(emptyMat)
+            dst2 = redWC.wcData.dst2
+
+            Dim regionlist = RedWC_Basics.countRegions(dst2, labels(3))
+            strOut = ""
+            Dim count As Integer
+            For i = 0 To regionlist.Count - 1
+                Dim index = regionlist(i)
+                dst0 = dst2.InRange(index, index)
+                Dim mean = redWC.wcData.prepData.reduced32s.Mean(dst0)
+
+                If CInt(mean(0)) Mod task.fOptions.ReductionSlider.Value = 0 Then
+                    Dim region = CInt(mean(0) / task.fOptions.ReductionSlider.Value)
+                    strOut += "region " + CStr(region) + " = " + Format(mean(0), fmt0) + "  "
+                    If i Mod 3 = 0 And i > 0 Then strOut += vbCrLf
+                    count += 1
+                End If
+            Next
+            SetTrueText(strOut, 3)
+            labels(2) = CStr(Count) + " regions were found"
+        End Sub
+    End Class
+
+
+
+
+
+
+
+    Public Class RedWC_ValidateY : Inherits TaskParent
+        Dim redWC As New RedWC_Y
+        Public Sub New()
+            If standalone Then OptionParent.findRadio("Y Reduction").Checked = True
+            desc = "Identify each WC region."
+        End Sub
+        Public Overrides Sub RunAlg(src As cv.Mat)
+            redWC.Run(emptyMat)
+            dst2 = redWC.wcData.dst2
+
+            Dim regionlist = RedWC_Basics.countRegions(dst2, labels(3))
+            strOut = ""
+            Dim count As Integer
+            For i = 0 To regionlist.Count - 1
+                Dim index = regionlist(i)
+                dst0 = dst2.InRange(index, index)
+                Dim mean = redWC.wcData.prepData.reduced32s.Mean(dst0)
+                If CInt(mean(0)) Mod task.fOptions.ReductionSlider.Value = 0 Then
+                    strOut += "region " + CStr(i) + " = " + Format(mean(0), fmt0) + "  "
+                    If i Mod 3 = 0 And i > 0 Then strOut += vbCrLf
+                    count += 1
+                End If
+            Next
+            SetTrueText(strOut, 3)
+            labels(2) = CStr(count) + " regions were found"
+        End Sub
+    End Class
+
+
+End Namespace
